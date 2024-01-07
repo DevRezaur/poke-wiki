f996ad5a6b894852789bc3d7246eea1a0ec5123e
Rezaur Rahman <rezaur.official@gmail.com>
2024-01-07 20:08:44 +0600

    API integration complete for pokemon-list page

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PokeApiService } from '../../services/poke-api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,19 +8,47 @@ import { PokeApiService } from '../../services/poke-api.service';
   styleUrl: './pokemon-list.component.scss',
 })
 export class PokemonListComponent implements OnInit {
+  currentPageNo: number;
   pokemonDetails: any;
 
-  constructor(private pokeApiService: PokeApiService) {
-    this.pokemonDetails = {};
+  constructor(
+    private pokeApiService: PokeApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.currentPageNo = 0;
+    this.pokemonDetails = [];
   }
 
   ngOnInit(): void {
-    // this.pokeApiService.getPokemonDetails(null).subscribe({
-    //   next: (result: any) => {
-    //     this.pokemonDetails = result;
-    //     console.log(this.pokemonDetails);
-    //   },
-    //   error: (error: any) => console.error(error),
-    // });
+    this.setCurrentPageNo();
+    this.loadPokemonDetails();
+  }
+
+  setCurrentPageNo(): void {
+    this.currentPageNo = this.route.snapshot.queryParams['page'] || 0;
+  }
+
+  loadPokemonDetails(): void {
+    this.pokeApiService.getPokemonDetails(this.currentPageNo).subscribe({
+      next: (result) => (this.pokemonDetails = result),
+      error: (error) => console.error(error),
+    });
+  }
+
+  loadPreviousPage(): void {
+    this.router
+      .navigate(['/pokemon-list'], {
+        queryParams: { page: --this.currentPageNo },
+      })
+      .finally(() => this.loadPokemonDetails());
+  }
+
+  loadNextPage(): void {
+    this.router
+      .navigate(['/pokemon-list'], {
+        queryParams: { page: ++this.currentPageNo },
+      })
+      .finally(() => this.loadPokemonDetails());
   }
 }
--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -10,26 +10,32 @@ export class PokeApiService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getPokemonDetails(url: string | null): Observable<any> {
+  getPokemonDetails(pageNo: number): Observable<any> {
+    const url = `${this.baseUrl}?offset=${pageNo * 20}&limit=20`;
+
     return this.fetchPokemonList(url).pipe(
-      switchMap((result: any) => this.fetchPokemonDetails(result))
+      switchMap((result) =>
+        forkJoin(
+          result.results.map((pokemon: any) =>
+            this.fetchIndividualPokemonDetails(pokemon.url)
+          )
+        )
+      )
     );
   }
 
-  fetchPokemonList(url: string | null): Observable<any> {
-    return this.httpClient.get<any>(url ? url : this.baseUrl);
+  fetchPokemonList(url: string): Observable<any> {
+    return this.httpClient.get<any>(url);
   }
 
   fetchPokemonDetails(pokemonList: any): any {
-    const observables = pokemonList.results.map((pokemon: any) =>
+    const observables = pokemonList.map((pokemon: any) =>
       this.fetchIndividualPokemonDetails(pokemon.url).pipe(
-        map((result: any) => ({ ...pokemon, ...result }))
+        map((result) => ({ ...pokemon, ...result }))
       )
     );
 
-    return forkJoin(observables).pipe(
-      map((result: any) => ((pokemonList.results = result), pokemonList))
-    );
+    return forkJoin(observables);
   }
 
   fetchIndividualPokemonDetails(url: string): Observable<any> {
